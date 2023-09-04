41a45aa5bec26d31c7ef9a429a7213559ba9df63
Enes Tunahan <enesstunahan@gmail.com>
2023-09-04 20:20:10 +0300

    Yetkilendirme işlemini test etmek için örnek bir controller oluşturuldu

---
+++ Practice-jwt/Controllers/BookController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Practice_jwt.Models;
+
+namespace Practice_jwt.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class BookController : ControllerBase
+    {
+        private readonly List<Book> books;
+        public BookController() 
+        {
+            books = new List<Book>()
+            {
+                new Book()
+                {
+                     Id= 1,
+                     Name = "Kürk Mantolu Madonna"
+                },
+                new Book()
+                {
+                    Id= 2,
+                    Name  ="Kuyucaklı Yusuf"
+                }
+            };
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> GetBooks()
+        {
+            return books;
+        }
+
+    }
+}
---
+++ Practice-jwt/Models/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace Practice_jwt.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
